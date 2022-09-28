e83240eb2aa494acc71ba683de1cfd2477af7399
K.Iwamura <39090490+killstealking@users.noreply.github.com>
2022-09-28 14:48:39 +0900

    feat: add type to balance (#4)

--- contract_master/bsc/contract/bep20.py
+++ contract_master/bsc/contract/bep20.py
@@ -18,4 +18,6 @@ class Bep20TokenContract(BaseContract):
             block_identifier=block_identifier
         )
         decimals = self.contract.functions.decimals().call(block_identifier=block_identifier)
-        return Balance(application="bsc", service="spot", token=self.address, balance=balance, decimals=decimals)
+        return Balance(
+            application="bsc", service="spot", type="common", token=self.address, balance=balance, decimals=decimals
+        )
